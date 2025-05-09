ad9edd8c010fdd71973e0d07b6af8e6ef39aeeb0
VinceT <37818562+Play3rZer0@users.noreply.github.com>
2025-05-09 16:05:40 +0800

    Update #2 (5/9/25) TotalRewardSize.py

--- TotalRewardSize.py
+++ TotalRewardSize.py
@@ -1,19 +1,26 @@
 # Program to calculate the total supply of Bitcoin based on halving cycle and reward sizes
+# This is the simple way to calculate the total Bitcoin supply, but is not exact
 
 def calculate_total_bitcoin_supply():
-    #Calculates the total Bitcoin that will ever be mined.
+    
+    # Calculates the total Bitcoin that will ever be mined based on the halving cycle and total reward sizes.
+
     initial_reward = 50.0
     halving_interval = 210000
     reward = initial_reward
     total_bitcoin = 0.0
 
+    # This loop will continue as long as the reward size is greater than 0.
+
     while reward > 0:
-        rewards_in_era = halving_interval * reward
-        total_bitcoin += rewards_in_era
+        rewards_in_cycle = halving_interval * reward
+        total_bitcoin += rewards_in_cycle
         reward /= 2
 
-    print(f"The total amount of Bitcoin that will ever be mined is: {total_bitcoin} BTC")
     return total_bitcoin
+
+# The total BTC supply using this program will return 21,000,000 BTC.
+# This is based on the total of all reward sizes multiplied with the 210,000 halving interval
 
 if __name__ == "__main__":
     total_btc = calculate_total_bitcoin_supply()
